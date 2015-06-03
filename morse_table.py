dea07de0cc28ebd5bfaac4d171534290ef0e7bac
Clayton Smith <argilo@gmail.com>
2015-06-02 23:41:06 -0400

    Add copyright notice.

--- morse_table.py
+++ morse_table.py
@@ -1,3 +1,22 @@
+# Copyright 2014 Clayton Smith
+#
+# This file is part of contest-sdr
+#
+# contest-sdr is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# contest-sdr is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with contest-sdr; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+
 morsetab = {
         'A': [1,0,1,1,1,0,0,0],                              'a': [1,0,1,1,1,0,0,0],
         'B': [1,1,1,0,1,0,1,0,1,0,0,0],                      'b': [1,1,1,0,1,0,1,0,1,0,0,0],
